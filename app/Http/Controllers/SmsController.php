cf0939cded29206586326ea1d57d3ada54384a16
expertteam11@gmail.com <vinod.tiwari@redsymboltechnologies.com>
2018-09-15 10:54:11 +0530

    first commit on 15th sept 2018

---
+++ app/Http/Controllers/SmsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+use Twilio\Jwt\ClientToken;
+
+class SmsController extends Controller
+{
+	public function sms(){
+		return view('sms.index');
+	}
+    public function sendSms(Request $request)
+    {
+        $accountSid = config('app.twilio')['TWILIO_ACCOUNT_SID'];
+        $authToken  = config('app.twilio')['TWILIO_AUTH_TOKEN'];
+        $appSid     = config('app.twilio')['TWILIO_APP_SID'];
+        $client = new Client($accountSid, $authToken);
+		
+		$post_data = $request->all();
+		/* echo '<pre>';
+		print_r($post_data);exit; */
+        try{
+            // Use the client to do fun stuff like send text messages!
+            $client->messages->create(
+				// the number you'd like to send the message to
+                $post_data['to'],
+				array(
+					// A Twilio phone number you purchased at twilio.com/console
+					'from' => '+15673202145',
+					// the body of the text message you'd like to send
+					'body' => $post_data['body']
+				)
+			);
+			return redirect()->back()->with(['success'=>'Message send successfully!']);
+		}catch (Exception $e){
+            //echo "Error: " . $e->getMessage();
+			return redirect()->back()->withErrors(['error'=>"Error: " . $e->getMessage()]);
+        }
+    }
+}
---
+++ resources/views/sms/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Send Message</div>
+				
+                <div class="panel-body">
+					@if(Session::has('success'))
+						<div class="alert alert-success">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close" style="cursor:pointer;"><span aria-hidden="true">×</span></button>
+							{{Session::get('success')}}
+						</div>
+					@endif
+                    <form class="form-horizontal" method="POST" action="{{ URL::asset('sms') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('to') ? ' has-error' : '' }}">
+                            <label for="to" class="col-md-4 control-label">To</label>
+
+                            <div class="col-md-6">
+                                <input id="to" type="text" class="form-control" name="to" value="{{ old('to') }}" required autofocus placeholder="mobile number with country code">
+
+                                @if ($errors->has('to'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('to') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>                  
+
+                        <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
+                            <label for="body" class="col-md-4 control-label">Body</label>
+
+                            <div class="col-md-6">
+                                <!--<input id="body" type="password" class="form-control" name="body" required>-->
+								<textarea name="body" id="body" class="form-control" required placeholder="the body of the text message you'd like to send"></textarea>
+                                @if ($errors->has('body'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('body') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Send Message
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
